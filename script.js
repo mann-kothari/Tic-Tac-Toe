c2e79ff09f029d2059d729e43aef6011f7b519e7
mann-kothari <mannkothari17@gmail.com>
2024-11-18 21:40:12 +0530

    Updates

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ const checkWin = () =>{
         if((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText)&&(boxtexts[e[1]].innerText === boxtexts[e[2]].innerText)&&(boxtexts[e[0]].innerText !== '')){
             document.querySelector('.Info').innerText = boxtexts[e[0]].innerText + " WON";
             isgameover = true;
+            document.querySelector('.imageBox').getElementsByTagName('img')[0].style.width = "200px" 
         }
     });
 }
@@ -47,6 +48,7 @@ Array.from(boxes).forEach(element =>{
             document.getElementsByClassName('Info')[0].innerText = "Turn for "+ turn;
             boxText.innerText = '';
             isgameover = false;
+            document.querySelector('.imageBox').getElementsByTagName('img')[0].style.width = "0"
         })
     });
 });
